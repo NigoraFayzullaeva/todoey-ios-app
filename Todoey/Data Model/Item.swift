16d32c1ea2fcc180cc0615f6c3049afcc5847875
Nigora <61627731+NigoraFayzullaeva@users.noreply.github.com>
2021-08-09 13:01:57 +0300

    finally

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Nigora on 09/08/21.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
